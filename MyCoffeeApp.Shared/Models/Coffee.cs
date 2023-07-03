4d67d20ebdf6924c8ffd40b2f6aee31fca6d6b4b
James Montemagno <james.montemagno@gmail.com>
2023-07-03 15:02:35 -0700

    Add swipe and context

--- MyCoffeeApp.Shared/Models/Coffee.cs
+++ MyCoffeeApp.Shared/Models/Coffee.cs
@@ -1,12 +1,20 @@
-﻿using SQLite;
+﻿using CommunityToolkit.Mvvm.Input;
+using SQLite;
+using System;
 
 namespace MyCoffeeApp.Shared.Models;
 
-public class Coffee
+public partial class Coffee
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     public string Roaster { get; set; }
     public string Name { get; set; }
     public string Image { get; set; }
+
+    [RelayCommand]
+    void Test()
+    {
+        Console.Write("Test");
+    }
 }
--- MyCoffeeApp/Views/CoffeeEquipmentCVPage.xaml.cs
+++ MyCoffeeApp/Views/CoffeeEquipmentCVPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MyCoffeeApp.ViewModels;
+﻿using MyCoffeeApp.Shared.Models;
+using MyCoffeeApp.ViewModels;
 
 namespace MyCoffeeApp.Views;
 public partial class CoffeeEquipmentCVPage : ContentPage
@@ -7,5 +8,33 @@ public partial class CoffeeEquipmentCVPage : ContentPage
     {
         InitializeComponent();
         BindingContext = vm;
+    }
+
+    private async void SwipeItem_Invoked(object sender, EventArgs e)
+    {
+        var item = sender as SwipeItem;
+        if (item is null)
+            return;
+
+        var coffee = item.BindingContext as Coffee;
+        if (coffee is null)
+            return;
+
+        await Shell.Current.DisplayAlert("Coffee Favorited",
+            coffee.Name, "OK");
+    }
+
+    private async void MenuFlyoutItem_Clicked(object sender, EventArgs e)
+    {
+        var item = sender as MenuFlyoutItem;
+        if (item is null)
+            return;
+
+        var coffee = item.BindingContext as Coffee;
+        if (coffee is null)
+            return;
+
+        await Shell.Current.DisplayAlert("Coffee Favorited",
+            coffee.Name, "OK");
     }
 }
